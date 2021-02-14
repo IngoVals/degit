f781116d0ddf74e94a11d5e2a9043c54b1563b8d
Ingólfur Valsson <ingovals@gmail.com>
2021-02-14 12:27:52 +0000

    Fixes lint rules.

--- next-app/src/pages/_app.tsx
+++ next-app/src/pages/_app.tsx
@@ -1,17 +1,16 @@
-import styles from '@assets/styles.css';
-import type { AppProps /*, AppContext */ } from 'next/app';
+import '@assets/styles.css';
+import { AppProps } from 'next/app';
 import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
         <title>Next App</title>
-        <link rel="stylesheet" href={styles} />
       </Head>
       <Component {...pageProps} />
     </>
   );
-}
+};
 
 export default MyApp;
