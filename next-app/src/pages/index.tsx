e87f174da09995487bea69407ab5e15322028f85
Ingólfur Valsson <ingovals@gmail.com>
2021-02-15 08:20:48 +0000

    Adds xwind and emotion

--- next-app/src/pages/index.tsx
+++ next-app/src/pages/index.tsx
@@ -1,6 +1,14 @@
+import styled from '@emotion/styled';
+import xw from 'xwind';
+
+const Area = styled.div(
+  xw`flex items-center justify-center min-h-screen bg-gray-200`
+);
+const Quote = styled.q(xw`italic text-gray-600`);
+
 const Home = (): JSX.Element => (
   <>
-    <div className="flex items-center justify-center min-h-screen bg-gray-200">
+    <Area>
       <div className="max-w-sm p-6 tracking-wide bg-white border-2 border-gray-300 rounded-md shadow-lg">
         <div id="header" className="flex items-center mb-4">
           <img
@@ -18,12 +26,10 @@ const Home = (): JSX.Element => (
           </div>
         </div>
         <div id="quote">
-          <q className="italic text-gray-600">
-            Initialized with typescript and tailwind.
-          </q>
+          <Quote>Initialized with typescript and tailwind.</Quote>
         </div>
       </div>
-    </div>
+    </Area>
   </>
 );
 
