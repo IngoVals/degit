059b1d262d74d2b5854dcb29ffb534091a3fa535
Ingólfur Valsson <ingovals@gmail.com>
2021-02-17 21:34:03 +0000

    Update to styled components

--- next-app/src/pages/index.tsx
+++ next-app/src/pages/index.tsx
@@ -5,32 +5,28 @@ const Area = styled.div(
   xw`flex items-center justify-center min-h-screen bg-gray-200`
 );
 const Quote = styled.q(xw`italic text-gray-600`);
+const Card = styled.div(
+  xw`max-w-sm p-6 tracking-wide bg-white border-2 border-gray-300 rounded-md shadow-lg`
+);
+const CardHeader = styled.div(xw`flex items-center mb-4`);
+const CardImage = styled.img(xw`w-20 border-2 border-gray-300 rounded-full`);
+const HeaderTextArea = styled.div(xw`ml-6 leading-5`);
+const Header4 = styled.h4(xw`text-xl font-semibold`);
+const Subheader = styled.h5(xw`font-semibold text-green-600`);
 
 const Home = (): JSX.Element => (
-  <>
-    <Area>
-      <div className="max-w-sm p-6 tracking-wide bg-white border-2 border-gray-300 rounded-md shadow-lg">
-        <div id="header" className="flex items-center mb-4">
-          <img
-            alt="avatar"
-            className="w-20 border-2 border-gray-300 rounded-full"
-            src="https://picsum.photos/seed/picture/200"
-          />
-          <div id="header-text" className="ml-6 leading-5 sm">
-            <h4 id="name" className="text-xl font-semibold">
-              Next app
-            </h4>
-            <h5 id="job" className="font-semibold text-blue-600">
-              React Framework
-            </h5>
-          </div>
-        </div>
-        <div id="quote">
-          <Quote>Initialized with typescript and tailwind.</Quote>
-        </div>
-      </div>
-    </Area>
-  </>
+  <Area>
+    <Card>
+      <CardHeader>
+        <CardImage alt="tower" src="https://picsum.photos/seed/nextjs/200" />
+        <HeaderTextArea>
+          <Header4>Next app</Header4>
+          <Subheader>React Framework</Subheader>
+        </HeaderTextArea>
+      </CardHeader>
+      <Quote>Initialized with typescript and tailwind.</Quote>
+    </Card>
+  </Area>
 );
 
 export default Home;
